3dc64a6eb26880f974cede2d4fbb5cff5f17f52a
VikasSPatel <i.vikaspatel@gmail.com>
2025-05-09 23:28:50 +0530

    Adds useGetPokemonDetails custom hook

---
+++ src/hooks/useGetPokemonDetails.ts
@@ -0,0 +1,43 @@
+// src/hooks/useGetPokemonDetails.ts
+import { gql, useQuery } from '@apollo/client';
+
+export const GET_POKEMON_DETAILS = gql`
+  query pokemon($id: String, $name: String) {
+    pokemon(id: $id, name: $name) {
+      id
+      number
+      name
+      weight {
+        minimum
+        maximum
+      }
+      height {
+        minimum
+        maximum
+      }
+      classification
+      types
+      resistant
+      weaknesses
+      fleeRate
+      maxCP
+      maxHP
+      image
+    }
+  }
+`;
+
+export const useGetPokemonDetails = (id?: string, name?: string) => {
+  const { data, loading, error } = useQuery(GET_POKEMON_DETAILS, {
+    variables: { id, name },
+    skip: !id && !name, // skip query if no id or name
+  });
+
+  console.log(data);
+
+  return {
+    pokemon: data?.pokemon,
+    loading,
+    error,
+  };
+};
